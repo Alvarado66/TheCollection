82e04ae981d81b2cb1d04d081302bbf4d6280971
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-10-21 22:06:16 -0400

    work to welcome box

---
+++ client/src/components/IntroContent/Intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Intro.css";
+
+function Intro() {
+    return(
+        <div className="welcomeBlock">
+            <div className="row">
+                
+            </div>
+            Welcome To Elevated Rustic
+        </div>
+    )
+}
+
+export default Intro;
