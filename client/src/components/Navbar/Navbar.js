6bc1b519d35a26639cdf946392ff142c86fd230f
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-11-09 20:53:27 -0500

    responsive w/menu

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 
 
 function Navbar() {
+
+    const [open, setOpen] = useState (false);
+
     return (
+        
         <>
+        <div>
         <nav>
-        <ul className="nav-links">
+            <div className="brandLogo"></div>
+        <ul className="nav-links" style={{ transform: open ? "translateX(0px)" : "translateX(-500px)" }}>
             <li><a>Home</a></li>
             <li><a>About Me</a></li>
             <li><a>Comments</a></li>
+            <li><a>Contact</a></li>
+            
         </ul>
-        <i className="fas fa-bars burgerMenu"></i>
+        <i onClick={() => setOpen(!open)} className="fas fa-bars burgerMenu"></i>
         </nav>
+        
+        </div>
+
         </>
     );
 }
