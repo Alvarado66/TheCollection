beed11d7af18b2f168b11ac518adce7203089511
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-11-09 21:07:50 -0500

    minor appearance tweak to Navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,25 +4,25 @@ import './Navbar.css';
 
 function Navbar() {
 
-    const [open, setOpen] = useState (false);
+    const [open, setOpen] = useState(false);
 
     return (
-        
+
         <>
-        <div>
-        <nav>
-            <div className="brandLogo"></div>
-        <ul className="nav-links" style={{ transform: open ? "translateX(0px)" : "translateX(-500px)" }}>
-            <li><a>Home</a></li>
-            <li><a>About Me</a></li>
-            <li><a>Comments</a></li>
-            <li><a>Contact</a></li>
-            
-        </ul>
-        <i onClick={() => setOpen(!open)} className="fas fa-bars burgerMenu"></i>
-        </nav>
-        
-        </div>
+            <div>
+                <nav>
+                    <div className="brandLogo">Awesome Dev! Co.</div>
+                    <ul className="nav-links" style={{ transform: open ? "translateX(0px)" : "translateX(-500px)" }}>
+                        <li><a>Home</a></li>
+                        <li><a>About Me</a></li>
+                        <li><a>Comments</a></li>
+                        <li><a>Contact</a></li>
+
+                    </ul>
+                    <i onClick={() => setOpen(!open)} className="fas fa-bars burgerMenu"></i>
+                </nav>
+
+            </div>
 
         </>
     );
