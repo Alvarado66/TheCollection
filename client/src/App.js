8dcc7dfac5c8d5c72a10c79e0248dfefb94e48ec
Fernando Alvarado- <Fernandoahd@icloud.com>
2020-11-16 20:15:47 -0500

    working on button feature

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import Intro from './components/IntroContent/Intro';
 import Navbar from './components/Navbar/Navbar';
+import justPick from './components/MainContent/MainContent';
+
 
 function App() {
   return (
-    <>
+   <>
     <Intro />
     <Navbar />
+    <justPick />
+  </>
   
-    </>
+    
   )
 };
 
--- client/src/components/MainContent/MainContent.js
+++ client/src/components/MainContent/MainContent.js
@@ -1,29 +1,12 @@
-import React, { useState } from 'react';
-
-
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
 function justPick() {
-
-    const [open, setOpen] = useState(false);
-
     return (
-
         <>
-            <div>
-                <nav>
-                    <div className="brandLogo">Awesome Dev! Co.</div>
-                    <ul className="nav-links" style={{ transform: open ? "translateX(0px)" : "translateX(-500px)" }}>
-                        <li><a>Home</a></li>
-                        <li><a>About Me</a></li>
-                        <li><a>Comments</a></li>
-                        <li><a>Contact</a></li>
-
-                    </ul>
-                    <i onClick={() => setOpen(!open)} className="fas fa-bars burgerMenu"></i>
-                </nav>
-
+            <div className="startSearch">
+                <Button>Hello</Button>
             </div>
-
         </>
     );
 }
